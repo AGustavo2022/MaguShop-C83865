f71a307673c8af65b9371e29f1f8ec5b38d4d0e5
Acosta Gustavo <gustavo080487@gmail.com>
2025-09-19 20:07:41 -0300

    se agrega una flatcard

--- src/screens/shop/ProductsScreen.jsx
+++ src/screens/shop/ProductsScreen.jsx
@@ -1,58 +1,52 @@
-import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native'
+import { StyleSheet, Text, View, FlatList,Pressable } from 'react-native'
 //import products from '../../data/products.json'
+import FlatCard from '../../components/FlatCard'
+import { colors } from '../../global/colors'
 import { useEffect, useState } from 'react'
-import KarlaRegularText from '../../components/KarlaRegularFont'
 import Search from '../../components/Search'
-import { useSelector,useDispatch } from 'react-redux'
-import { setProductSelected } from '../../store/slices/shopSlice'
+import { useSelector } from 'react-redux'
 import { useGetProductsByCategoryQuery } from '../../services/shopApi'
 
-const ProductsScreen = ({ navigation, route }) => {
+
+const ProductsScreen = ({ navigation }) => {
     const [productsFiltered, setProductsFiltered] = useState([])
     const [keyword, setKeyword] = useState("")
 
-    //console.log("Keyword: ",keyword)
-
-    //const { category } = route.params
+    const products = useSelector(state=>state.shopReducer.products)
     const category = useSelector(state=>state.shopReducer.categorySelected)
 
-    const {data:productsFilteredByCategory, isLoading, error} = useGetProductsByCategoryQuery(category.toLowerCase())
-
-    const dispatch = useDispatch()
-
-    const handleSelectProduct = (product) => {
-        dispatch(setProductSelected(product))
-        navigation.navigate("Producto")
-    }
-
-    const renderProductsItem = ({ item }) => (
-        <View>
-            <Pressable onPress={() => handleSelectProduct(item)}>
-                <KarlaRegularText style={{ fontSize: 16 }}>{item.title}</KarlaRegularText>
-            </Pressable>
-        </View>
-
-    )
+    const {data: productsFilteredByCategory, isLoading, error} = useGetProductsByCategoryQuery(category.toLowerCase())
 
     useEffect(() => {
-        //const productsFilteredByCategory = products.filter(product => product.category.toLowerCase() === category.toLowerCase())
-        if (keyword) { //Re-filtramos la lista de productos según la búsqueda del usuario
-            const productsFilteredByKeyword = productsFilteredByCategory.filter(product => product.title.toLowerCase().includes(keyword.toLocaleLowerCase()))
+        if (keyword) {
+            const productsFilteredByKeyword = productsFilteredByCategory.filter(
+                product => product.title.toLowerCase().includes(keyword.toLowerCase())
+            )
             setProductsFiltered(productsFilteredByKeyword)
         } else {
             setProductsFiltered(productsFilteredByCategory)
         }
     }, [category, keyword,productsFilteredByCategory])
 
+    const renderProductItem = ({ item }) => (
+        <Pressable onPress={()=>navigation.navigate("Producto",{product:item})}>
+            <FlatCard>
+                <Text>{item.title}</Text>
+            </FlatCard>
+        </Pressable>
+
+    )
+
     return (
-        <View >
-            <Search setKeyword={setKeyword} />
+        <>
+            <Search keyword={keyword} setKeyword={setKeyword} />
             <FlatList
                 data={productsFiltered}
+                renderItem={renderProductItem}
                 keyExtractor={item => item.id}
-                renderItem={renderProductsItem}
             />
-        </View>
+        </>
+
     )
 }
 
