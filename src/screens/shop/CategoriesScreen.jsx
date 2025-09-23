86274183b8a9eea60ebc1b31630d364378704c67
Acosta Gustavo <gustavo080487@gmail.com>
2025-09-23 18:18:53 -0300

    se modifica la card de categorias

--- src/screens/shop/CategoriesScreen.jsx
+++ src/screens/shop/CategoriesScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, FlatList,Pressable } from 'react-native'
+import { Pressable, View, StyleSheet, Image, Dimensions, FlatList } from 'react-native';
 import FlatCard from '../../components/FlatCard';
 import { useDispatch } from 'react-redux';
 import { setCategorySelected } from '../../store/slices/shopSlice';
@@ -6,28 +6,38 @@ import { useGetCategoriesQuery } from '../../services/shopApi';
 import JosefinSansRegular from '../../components/JosefinSans-Regular';
 
 
-const CategoriesScreen = ({navigation}) => {
+const CategoriesScreen = ({ navigation }) => {
 
-    const {data:categories, isLoading, error} = useGetCategoriesQuery()
+    const { data: categories, isLoading, error } = useGetCategoriesQuery()
 
 
     const dispatch = useDispatch()
 
-    const handleSelectCategory = (category)=>{
+    const screenWidth = Dimensions.get('window').width;
+    const cardWidth = screenWidth * 0.40; // Por ejemplo, 40% del ancho
+
+    const handleSelectCategory = (category) => {
         dispatch(setCategorySelected(category))
         navigation.navigate("Productos")
     }
 
     const renderCategoryItem = ({ item }) => {
         return (
-            <Pressable onPress={()=>handleSelectCategory(item.title)}>
-                <FlatCard style={styles.cardCustom}>
+            <Pressable onPress={() => handleSelectCategory(item.title)}>
+                <FlatCard style={[styles.cardCustom, { width: cardWidth }]}>
+                    <View style={styles.iconContainer}>
+                        <Image
+                            source={{ uri: item.image }}
+                            style={styles.categoryIcon}
+                            resizeMode='contain'
+                        />
+                    </View>
                     <JosefinSansRegular style={styles.title}>{item.title}</JosefinSansRegular>
-                    <Image width={120} height={80} source={{ uri: item.image }} resizeMode='contain' />
                 </FlatCard>
             </Pressable>
         )
     }
+
     return (
         <FlatList
             data={categories}
@@ -39,10 +49,34 @@ const CategoriesScreen = ({navigation}) => {
 
 export default CategoriesScreen
 
+
 const styles = StyleSheet.create({
     cardCustom: {
-
-    },title:{
-        marginRight: 16
-    }
-})
+        padding: 10,
+        margin: 8,
+        borderRadius: 12,
+        alignItems: 'center', 
+        justifyContent: 'space-between', 
+        height: 150, 
+        elevation: 5,
+    }, 
+    iconContainer: {
+        width: 70, 
+        height: 70,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000', 
+        shadowOpacity: 0.05, 
+        shadowRadius: 2,
+    },
+    categoryIcon: {
+        width: 100,
+        height: 80,
+    },
+    title: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginTop: 5,
+    },
+});
